9d2563252cd78ee0a39bc3bc995784e48b626194
r351574nc3 <r351574nc3@gmail.com>
2014-12-08 20:18:24 -0700

    Adding LatLngBounds implementation.

--- LatLngBounds.c
+++ LatLngBounds.c
@@ -12,6 +12,28 @@ void latlngbounds_free(LatLngBounds * tofree) {
     free(tofree);
 }
 
-int latlngbounds_contains(LatLng * point, LatLngBounds * LatLngBounds) {
-    return 0;
+int latlngbounds_contains(LatLng * point, LatLngBounds * bounds) {
+    int retval = 1;
+    if (point->lat > 0) {
+        if (point->lat >= bounds->ne->lat) {
+            return false;
+        }
+    }
+    else {
+        if (point->lat <= bounds->sw->lat) {
+            return false;
+        }
+    }
+
+    if (point->lng > 0) {
+        if (point->lng >= bounds->ne->lng) {
+            return false;
+        }
+    }
+    else {
+        if (point->lng <= bounds->sw->lng) {
+            return false;
+        }
+    }
+    return retval;
 }
